d77fe6bee060d08c2812baf238ecdeaabe475659
simple <jb.xue@qq.com>
2019-07-08 18:10:51 +0800

    ...

--- DesignPattern/SingletonPattern/SingletonPattern.cs
+++ DesignPattern/SingletonPattern/SingletonPattern.cs
@@ -11,10 +11,7 @@ namespace DesignPattern.SingletonPattern
     {
         private static SingletonV1 instance;
 
-        private SingletonV1()
-        {
-
-        }
+        private SingletonV1() { }
 
         public static SingletonV1 getInstance()
         {
@@ -28,19 +25,19 @@ namespace DesignPattern.SingletonPattern
 
     public sealed class SingletonV2
     {
-        private static SingletonV3 instance;
+        private static SingletonV2 instance;
 
-        //静态制度的进程辅助对象
+        //静态只读的进程辅助对象
         private static readonly object syncObject = new object();
 
-        public SingletonV3 getInstance()
+        public SingletonV2 getInstance()
         {           
             //在同一时刻加了锁的那部分程序只有一个线程可以进入
             lock (syncObject)
             {
                 if (instance == null)
                 {
-                    instance = new SingletonV3();
+                    instance = new SingletonV2();
                 }
             }
             return instance;
@@ -51,7 +48,7 @@ namespace DesignPattern.SingletonPattern
     {
         private static SingletonV3 instance;
 
-        //静态制度的进程辅助对象
+        //静态只读的进程辅助对象
         private static readonly object syncObject = new object();
 
         public SingletonV3 getInstance()
@@ -77,13 +74,14 @@ namespace DesignPattern.SingletonPattern
            在实际应用中，C#与公共语言运行库也提供了一种‘静态初始化’方法，
        这种方法不需要开发人员显式地编写线程安全代码，即可解决多线程环境下
        它是不安全的问题。
-       //*/
+       */
         private static readonly SingletonV4 instance = new SingletonV4();
 
-        static SingletonV4()
+       /*static SingletonV4()
         {
             instance = new SingletonV4();
         }
+        */
 
         private SingletonV4()
         {
